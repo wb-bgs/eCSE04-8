00d1505a7a393c006c161d55ae2f2494d647ba13
Michael Bareford <m.bareford@epcc.ed.ac.uk>
2023-03-08 12:58:29 +0000

    Minor edit

--- tests/WMAM/shd/scripts/make_binary_files.sh
+++ tests/WMAM/shd/scripts/make_binary_files.sh
@@ -4,6 +4,6 @@ SHD=$1
 
 module -q load cray-python
 
-python make_binary_file.py ${HOME/home/work}/tests/WMAM/shd/${SHD} coef_1990_15.dat
-python make_binary_file.py ${HOME/home/work}/tests/WMAM/shd/${SHD} model.in
-python make_binary_file.py ${HOME/home/work}/tests/WMAM/shd/${SHD} wdmam_geocentric.dat
+python make_binary_file.py /work/ecsead08/ecsead08/mrbad08/tests/WMAM/shd/${SHD} coef_1990_15.dat
+python make_binary_file.py /work/ecsead08/ecsead08/mrbad08/tests/WMAM/shd/${SHD} model.in
+python make_binary_file.py /work/ecsead08/ecsead08/mrbad08/tests/WMAM/shd/${SHD} wdmam_geocentric.dat
