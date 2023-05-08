5565d2d4ed734c9cc5af96d7f0b5a1b6e1d9b9da
Michael Bareford <m.bareford@epcc.ed.ac.uk>
2023-05-08 10:50:20 +0100

    Changed 3.6 to 3.7.

--- tests/WMAM/scripts/plotting/plot_misc.py
+++ tests/WMAM/scripts/plotting/plot_misc.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.gcf()
 
+
 shdeg  = [ 200,  300,  720, 1440, 2000]
 resdeg = [1.00, 0.50, 0.25, 0.10, 0.05]
 
@@ -23,44 +24,55 @@ logncoeffs = np.log10(ncoeffs)
 shdeg_100 = [int(shd/100) if shd/100 % 1 == 0 else shd/100 for shd in shdeg]
 
 
-plt.plot(shdeg_100,lognpts,linestyle="",marker="o",color="#fc8d62")
-plt.xlabel('Spherical Harmonic Degree / 100')
+plt.plot(shdeg_100,logncoeffs,linestyle="-",marker="o",color="#fc8d62")
+plt.xlabel('Degree / 100')
 plt.xticks(shdeg_100, shdeg_100)
 plt.ylabel('Log10')
-plt.title('WMAM Total Point Count')
+plt.title(r'WMAM Coefficient Count ($N_c$)')
 plt.show()
-fig.savefig('wmam-pt-cnt.eps', format='eps', dpi=1000)
 
-plt.clf()
-
-plt.plot(shdeg_100,samptfrac,linestyle="",marker="o",color="#fc8d62")
-plt.xlabel('Spherical Harmonic Degree')
-plt.xticks(shdeg_100, shdeg_100)
-plt.ylabel('Percentage')
-plt.title('Percentage of WMAM Points Sampled')
-plt.show()
-fig.savefig('wmam-sampt-frac.eps', format='eps', dpi=1000)
-
-plt.clf()
-
-plt.plot(shdeg_100,logncoeffs,linestyle="",marker="o",color="#fc8d62")
-plt.xlabel('Spherical Harmonic Degree')
-plt.xticks(shdeg_100, shdeg_100)
-plt.ylabel('Log10')
-plt.title('WMAM Number of Coefficients')
-plt.show()
 fig.savefig('wmam-coeff-cnt.eps', format='eps', dpi=1000)
-
+plt.show()
 plt.clf()
+fig = plt.gcf()
 
-memreq1 = [0.9, 2.6, 12.7, 62.3, 187.7]
-memreq2 = [0.2, 0.5, 3.1, 12.4, 24.3]
-plt.plot(shdeg_100,memreq1,linestyle="",marker="o",label="<= WMAM v2.1",color="#fc8d62")
-plt.plot(shdeg_100,memreq2,linestyle="",marker="P",label=">= WMAM v2.2",color="#8da0cb")
-plt.xlabel('Spherical Harmonic Degree')
+plt.plot(shdeg_100,lognpts,linestyle="-",marker="o",color="#fc8d62")
+plt.xlabel('Degree / 100')
+plt.xticks(shdeg_100, shdeg_100)
+plt.ylabel('Log10')
+plt.title(r'WMAM Point Count ($N_p$)')
+plt.show()
+
+fig.savefig('wmam-pt-cnt.eps', format='eps', dpi=1000)
+plt.show()
+plt.clf()
+fig = plt.gcf()
+
+plt.plot(shdeg_100,samptfrac,linestyle="-",marker="o",color="#fc8d62")
+plt.xlabel('Degree / 100 ')
 plt.xticks(shdeg_100, shdeg_100)
 plt.ylabel('Percentage')
-plt.title('WMAM ARCHER2 Node Memory Requirement')
+plt.title('WMAM Points Sampled Percentage')
+plt.show()
+
+fig.savefig('wmam-sampt-frac.eps', format='eps', dpi=1000)
+plt.show()
+plt.clf()
+fig = plt.gcf()
+
+memreq1 = [39.123, 39.524, 42.956, 55.407, 252.025]
+memreq2 = [0.248, 0.56, 3.197, 12.858, 25.297]
+
+plt.plot(shdeg_100,memreq1,linestyle="-",marker="o",label="WMAM v1.2",color="#fc8d62")
+plt.plot(shdeg_100,memreq2,linestyle="-",marker="P",label="WMAM v3.7",color="#8da0cb")
+plt.xlabel('Degree')
+plt.xticks(shdeg_100, shdeg_100)
+plt.ylabel('Percentage')
+plt.title('WMAM Memory Requirement as percentage of 256 GB')
 plt.legend(loc="upper left")
 plt.show()
-fig.savefig('wmam-a2mem-req.eps', format='eps', dpi=1000)
+
+fig.savefig('wmam-mem-req.eps', format='eps', dpi=1000)
+plt.show()
+plt.clf()
+fig = plt.gcf()
