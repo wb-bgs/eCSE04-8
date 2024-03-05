37a64122c8d943ac5723625b6eac924020294d6c
Michael Bareford <mrb@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2024-03-05 16:36:57 +0000

    Added OMP offload directives with #ifdef guards.

--- libs/globlibi/src/cpt_dat_vals_p.f
+++ libs/globlibi/src/cpt_dat_vals_p.f
@@ -13,14 +13,12 @@ c         nt            basis number for each point
 c         ppos(nd+1,*)  point position in ndD
 c         nb            Number of base functions
 c         bc            base coefficients
-c         sub_base      Base subroutine to use
 c
 c       output:
 c         xyzf(*)       X,Y,Z or F value at point position
 c        
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-        subroutine cpt_dat_vals_p(nd, np, nt, ppos, nb, bc,
-     >                            sub_base, xyzf)
+        subroutine cpt_dat_vals_p(nd, np, nt, ppos, nb, bc, xyzf)
 c
         implicit none
 c
@@ -31,12 +29,11 @@ c
         integer :: i
         real*8, allocatable :: dw1(:)
 c
-        external sub_base
 c
         allocate(dw1(nb))
 c
         do i=1,np
-          call sub_base('f',nt,nb,bc,ppos(1,i),dw1)
+          call sph_bi('f',nt,nb,bc,ppos(1,i),dw1)
           xyzf(i) = dw1(1)
         enddo
 c
