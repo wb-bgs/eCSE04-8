b0e85d49fc8af0c85225f6b5b8079e1be6d5c2f6
user name <mrb@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2025-01-17 15:08:25 +0000

    Added two GPU kernels to handle data and sampled points.

--- apps/WMAM/src/globlibi/cpt_dat_vals_p.f
+++ apps/WMAM/src/globlibi/cpt_dat_vals_p.f
@@ -27,82 +27,115 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c
         implicit none
 c
-        integer shdeg, nb, nd, nlocpts, nlocdatpts
-        real*8  d2a(0:shdeg)
-        real*8  dlf(1:shdeg+1), ddlf(1:shdeg+1)
-        real*8  bc(1:nb), ppos(nd+1,nlocpts)
-        real*8  xyzf(1:nlocpts)
+        integer, unified :: shdeg, nb, nd, nlocpts, nlocdatpts
+        real(8), unified :: d2a(0:shdeg)
+        real(8), unified :: dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real(8), unified :: bc(1:nb), ppos(nd+1,nlocpts)
+        real(8), unified :: xyzf(1:nlocpts)
 c
-        real*8, parameter :: RAG = 6371.2d0
-        real*8, parameter :: D2R = 4.d0*datan(1.d0)/180.d0
-c        
+        integer n_threads, n_blocks
+c
+c
+        n_threads = 128
+c
+        n_blocks = nlocdatpts / n_threads
+        call cpt_dat_vals_p_dat<<<n_blocks,n_threads>>>
+     >    (shdeg, nb, nd, nlocpts, nlocdatpts,
+     >     d2a, dlf, ddlf, bc, ppos, xyzf)
+c
+        n_blocks = (nlocpts - nlocdatpts) / n_threads
+        call cpt_dat_vals_p_smp<<<n_blocks,n_threads>>>
+     >    (shdeg, nb, nd, nlocpts, nlocdatpts,
+     >     d2a, dlf, ddlf, bc, ppos, xyzf)
+c
+        end subroutine cpt_dat_vals_p
+c
+c
+c
+        attributes(global)
+     >  subroutine cpt_dat_vals_p_dat(shdeg, nb, nd,
+     >                                nlocpts, nlocdatpts,
+     >                                d2a, dlf, ddlf,
+     >                                bc, ppos, xyzf)
+c
+        use XYZsph_bi0
+c
+        implicit none
+c
+        integer shdeg, nb, nd
+        integer nlocpts, nlocdatpts
+        real(8) d2a(0:shdeg)
+        real(8) dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real(8) bc(1:nb), ppos(nd+1,nlocpts)
+        real(8) xyzf(1:nlocpts)
+c
+        real(8), parameter :: RAG = 6371.2d0
+        real(8), parameter :: D2R = 4.d0*datan(1.d0)/180.d0
+c
         integer i
-        real*8 p1, p2, ra
-        real*8 bex, bey, bez
 c
-        real*8 XYZsph_bi0_fun
-c
-c
-#if defined(OMP_OFFLOAD_CPTP)
-!$OMP TARGET DATA
-!$omp& map(to: bc(1:nb))
-!$omp& map(tofrom: xyzf(1:nlocpts))
-#endif
+        real(8) p1, p2, ra
+        real(8) bex, bey, bez
 c
 c
-#if defined(OMP_OFFLOAD_CPTP)
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP PARALLEL DO
-#endif
-!$omp& default(shared)
-!$omp& private(dlf, ddlf)
-!$omp& private(p1, p2, ra)
-!$omp& private(bex, bey, bez)
-#if defined(OMP_OFFLOAD_CPTP)
-!$omp& dist_schedule(static)
-#else
-!$omp& schedule(static)
-#endif
-        do i = 1,nlocdatpts
+        i = (blockidx%x-1) * blockdim%x
+     >      + threadidx%x
 c
-           p1 = ppos(1,i)*D2R
-           p2 = ppos(2,i)*D2R
-           ra = RAG / ppos(3,i)
+        if (i .ge. 1 .and.
+     >      i .le. nlocdatpts) then
 c
-           bex = ppos(5,i)
-           bey = ppos(6,i)
-           bez = ppos(7,i)
+          p1 = ppos(1,i)*D2R
+          p2 = ppos(2,i)*D2R
+          ra = RAG / ppos(3,i)
 c
-           xyzf(i) = XYZsph_bi0_fun(shdeg, nb, d2a,
-     >                              dlf, ddlf,
-     >                              bc, p1, p2, ra,
-     >                              bex, bey, bez)
+          bex = ppos(5,i)
+          bey = ppos(6,i)
+          bez = ppos(7,i)
 c
-        enddo
-#if defined(OMP_OFFLOAD_CPTP)
-!$OMP END TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP END PARALLEL DO
-#endif
+          xyzf(i) = XYZsph_bi0_fun(shdeg, nb, d2a,
+     >                             dlf, ddlf,
+     >                             bc, p1, p2, ra,
+     >                             bex, bey, bez)
 c
+        endif
 c
-#if defined(OMP_OFFLOAD_CPTP)
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP PARALLEL DO
-#endif
-!$omp& default(shared)
-!$omp& private(dlf, ddlf)
-!$omp& private(p1, p2, ra)
-!$omp& private(bex, bey, bez)
-#if defined(OMP_OFFLOAD_CPTP)
-!$omp& dist_schedule(static)
-#else
-!$omp& schedule(static)
-#endif
-        do i = nlocdatpts+1,nlocpts
+        end subroutine cpt_dat_vals_p_dat
+   
+   
+        attributes(global)
+     >  subroutine cpt_dat_vals_p_smp(shdeg, nb, nd,
+     >                                nlocpts, nlocdatpts,
+     >                                d2a, dlf, ddlf,
+     >                                bc, ppos, xyzf)
 c
+        use XYZsph_bi0
+c 
+        implicit none
+c
+        integer shdeg, nb, nd
+        integer nlocpts, nlocdatpts
+c
+        real(8) d2a(0:shdeg)
+        real(8) dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real(8) bc(1:nb), ppos(nd+1,nlocpts)
+        real(8) xyzf(1:nlocpts)
+c
+        real(8), parameter :: RAG = 6371.2d0
+        real(8), parameter :: D2R = 4.d0*datan(1.d0)/180.d0
+c
+        integer i
+c
+        real(8) p1, p2, ra
+        real(8) bex, bey, bez
+c
+c   
+        i = (blockidx%x-1) * blockdim%x
+     >      + threadidx%x
+     >      + nlocdatpts
+c
+        if (i .gt. nlocdatpts .and. 
+     >      i .le. nlocpts) then
+
           p1 = ppos(1,i)*D2R
           p2 = ppos(2,i)*D2R
           ra = RAG / ppos(3,i)
@@ -121,15 +154,6 @@ c
      >                             p1, p2, ra,
      >                             bex, bey, bez)
 c
-        enddo
-#if defined(OMP_OFFLOAD_CPTP)
-!$OMP END TARGET TEAMS DISTRIBUTE PARALLEL DO
+        endif
 c
-!$OMP END TARGET DATA
-#else
-!$OMP END PARALLEL DO
-#endif
-c
-c
-        return
-        end
+        end subroutine cpt_dat_vals_p_smp
--- apps/WMAM/src/globlibi/ssqgh_dp.f
+++ apps/WMAM/src/globlibi/ssqgh_dp.f
@@ -41,58 +41,93 @@ c
 c
         include 'mpif.h'
 c
-        integer shdeg, nb, nd, nlocpts, nlocdatpts
-        real*8 d2a(0:shdeg)
-        real*8 dlf(1:shdeg+1), ddlf(1:shdeg+1)
-        real*8 bc(1:nb)
-        real*8 ppos(1:nd+1,1:nlocpts)
-        real*8 cov(1:nlocpts)
-        integer jcov(1:nlocpts+2)
-        real*8 ddat(1:nlocpts)
-        real*8 xyzf(1:nlocpts)
-        real*8 gj(1:nb), dh(1:nb)
+        integer, unified :: shdeg, nb, nd, nlocpts, nlocdatpts
+        real*8, unified :: d2a(0:shdeg)
+        real*8, unified :: dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real*8, unified :: bc(1:nb)
+        real*8, unified :: ppos(1:nd+1,1:nlocpts)
+        real*8, unified :: cov(1:nlocpts)
+        integer, unified :: jcov(1:nlocpts+2)
+        real*8, unified :: ddat(1:nlocpts)
+        real*8, unified :: xyzf(1:nlocpts)
+        real*8, unified :: gj(1:nb), dh(1:nb)
 c
-        real*8, parameter :: RAG = 6371.2d0
-        real*8, parameter :: D2R = 4.d0*datan(1.d0)/180.d0
-c
-        integer i, j, nu, ierr
-        real*8 p1, p2, ra
-        real*8 bex, bey, bez
-c
-        real*8 dw_dh, dw_gj
+        integer n_threads, n_blocks, ierr  
 c
 c
         gj(1:nb) = 0.0d0
         dh(1:nb) = 0.0d0
 c
-c       
-#if defined(OMP_OFFLOAD_SSQGH)
-!$OMP TARGET DATA
-!$omp& map(to: ddat(1:nlocpts))
-!$omp& map(to: xyzf(1:nlocpts))
-!$omp& map(to: bc(1:nb))
-!$omp& map(tofrom: gj(1:nb), dh(1:nb))
-#endif
+c
+        n_threads = 128
+c
+        n_blocks = nlocdatpts / n_threads
+        call ssqgh_dp_dat<<<n_blocks,n_threads>>>
+     >    (shdeg, nb, nd, nlocpts, nlocdatpts,
+     >     d2a, dlf, ddlf, bc, ppos,
+     >     cov, jcov, ddat, xyzf,
+     >     gj, dh)
+c
+        n_blocks = (nlocpts - nlocdatpts) / n_threads
+        call ssqgh_dp_smp<<<n_blocks,n_threads>>>
+     >    (shdeg, nb, nd, nlocpts, nlocdatpts,
+     >     d2a, dlf, ddlf, bc, ppos,
+     >     cov, jcov, ddat, xyzf,
+     >     gj, dh)
 c
 c
-#if defined(OMP_OFFLOAD_SSQGH)
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP PARALLEL DO
-#endif
-!$omp& default(shared)
-!$omp& private(dlf, ddlf)
-!$omp& private(p1, p2, ra)
-!$omp& private(bex, bey, bez)
-!$omp& private(dw_dh, dw_gj)
-#if defined(OMP_OFFLOAD_SSQGH)
-!$omp& dist_schedule(static)
-#else
-!$omp& schedule(static)
-!$omp& reduction(+:gj,dh)
-#endif
-        do i = 1,nlocdatpts
-c       
+        call MPI_ALLREDUCE(MPI_IN_PLACE, gj, nb,
+     >                     MPI_DOUBLE_PRECISION,
+     >                     MPI_SUM, MPI_COMM_WORLD, ierr)
+c
+        call MPI_ALLREDUCE(MPI_IN_PLACE, dh, nb,
+     >                     MPI_DOUBLE_PRECISION,
+     >                     MPI_SUM, MPI_COMM_WORLD, ierr)
+c
+c
+        end subroutine ssqgh_dp
+c
+c
+c
+        attributes(global)
+     >  subroutine ssqgh_dp_dat(shdeg, nb, nd,
+     >                          nlocpts, nlocdatpts,
+     >                          d2a, dlf, ddlf, bc, ppos,
+     >                          cov, jcov, ddat, xyzf,
+     >                          gj, dh)
+c
+        use XYZsph_bi0
+c
+        implicit none
+c
+        integer shdeg, nb, nd, nlocpts, nlocdatpts
+        real(8) d2a(0:shdeg)
+        real(8) dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real(8) bc(1:nb)
+        real(8) ppos(1:nd+1,1:nlocpts)
+        real(8) cov(1:nlocpts)
+        integer jcov(1:nlocpts+2)
+        real(8) ddat(1:nlocpts)
+        real(8) xyzf(1:nlocpts)
+        real(8) gj(1:nb), dh(1:nb)
+c
+        real(8), parameter :: RAG = 6371.2d0
+        real(8), parameter :: D2R = 4.d0*datan(1.d0)/180.d0
+c
+        integer i
+c
+        real(8) p1, p2, ra
+        real(8) bex, bey, bez
+c
+        real(8) dw_dh, dw_gj
+c
+c
+        i = (blockidx%x-1) * blockdim%x
+     >      + threadidx%x
+c
+        if (i .ge. 1 .and.
+     >      i .le. nlocdatpts) then
+c        
           p1 = ppos(1,i)*D2R
           p2 = ppos(2,i)*D2R
           ra = RAG / ppos(3,i)
@@ -114,31 +149,51 @@ c
      >                        dw_gj, dw_dh,
      >                        gj, dh)
 c
-        enddo
-#if defined(OMP_OFFLOAD_SSQGH)
-!$OMP END TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP END PARALLEL DO
-#endif
+        endif
+c
+        end subroutine ssqgh_dp_dat
 c
 c
-#if defined(OMP_OFFLOAD_SSQGH)
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO
-#else
-!$OMP PARALLEL DO
-#endif
-!$omp& default(shared)
-!$omp& private(dlf, ddlf)
-!$omp& private(p1, p2, ra)
-!$omp& private(bex, bey, bez)
-!$omp& private(dw_dh, dw_gj)
-#if defined(OMP_OFFLOAD_SSQGH)
-!$omp& dist_schedule(static)
-#else
-!$omp& schedule(static)
-!$omp& reduction(+:gj,dh)
-#endif
-        do i = nlocdatpts+1,nlocpts
+c
+        attributes(global)
+     >  subroutine ssqgh_dp_smp(shdeg, nb, nd,
+     >                          nlocpts, nlocdatpts,
+     >                          d2a, dlf, ddlf, bc, ppos,
+     >                          cov, jcov, ddat, xyzf,
+     >                          gj, dh)
+c
+        use XYZsph_bi0
+c
+        implicit none
+c
+        integer shdeg, nb, nd, nlocpts, nlocdatpts
+        real(8) d2a(0:shdeg)
+        real(8) dlf(1:shdeg+1), ddlf(1:shdeg+1)
+        real(8) bc(1:nb)
+        real(8) ppos(1:nd+1,1:nlocpts)
+        real(8) cov(1:nlocpts)
+        integer jcov(1:nlocpts+2)
+        real(8) ddat(1:nlocpts)
+        real(8) xyzf(1:nlocpts)
+        real(8) gj(1:nb), dh(1:nb)
+c
+        real(8), parameter :: RAG = 6371.2d0
+        real(8), parameter :: D2R = 4.d0*datan(1.d0)/180.d0
+c
+        integer i
+c
+        real(8) p1, p2, ra
+        real(8) bex, bey, bez
+c
+        real(8) dw_dh, dw_gj
+c
+c
+        i = (blockidx%x-1) * blockdim%x
+     >      + threadidx%x
+     >      + nlocdatpts
+
+        if (i .gt. nlocdatpts .and. 
+     >      i .le. nlocpts) then
 c
           p1 = ppos(1,i)*D2R
           p2 = ppos(2,i)*D2R
@@ -147,12 +202,12 @@ c
           bex = ppos(5,i)
           bey = ppos(6,i)
           bez = ppos(7,i)
-c
+
           call XYZsph_bi0_sample(shdeg, nb, d2a,
      >                           dlf, ddlf, bc,
      >                           p1, p2, ra, 
      >                           bex, bey, bez)
-c        
+c
 c  calculate the equations of condition   
 c  and update the G matrix and B vector 
 c
@@ -166,24 +221,6 @@ c
      >                        dw_gj, dw_dh,
      >                        gj, dh)
 c
-        enddo
-#if defined(OMP_OFFLOAD_SSQGH)
-!$OMP END TARGET TEAMS DISTRIBUTE PARALLEL DO
+        endif
 c
-!$OMP END TARGET DATA
-#else
-!$OMP END PARALLEL DO
-#endif
-c
-c
-        call MPI_ALLREDUCE(MPI_IN_PLACE, gj, nb,
-     >                     MPI_DOUBLE_PRECISION,
-     >                     MPI_SUM, MPI_COMM_WORLD, ierr)
-c
-        call MPI_ALLREDUCE(MPI_IN_PLACE, dh, nb,
-     >                     MPI_DOUBLE_PRECISION,
-     >                     MPI_SUM, MPI_COMM_WORLD, ierr)
-c
-c
-        return
-        end
+        end subroutine ssqgh_dp_smp
