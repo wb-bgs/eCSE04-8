fb958223c3a58b6fd37cd9734b2f63813abe5e1b
user name <mrb@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2025-01-22 16:27:04 +0000

    Added CUDA_DEBUG flag.

--- apps/WMAM/build-cirrus-nvfortran.sh
+++ apps/WMAM/build-cirrus-nvfortran.sh
@@ -4,9 +4,9 @@
 function set_compile_options {
   MAKEFILE=$1
   if [[ "${BUILD}" == "release" ]]; then
-    sed -i "s:FFLAGS =:FFLAGS = -I${MPI_HOME}/include -O3 -cpp -r8 -tp=cascadelake -target=gpu -mp=gpu -Minfo=mp -gpu=cuda12.4,cc70,mem\:unified -cuda:g" ${MAKEFILE}
+    sed -i "s:FFLAGS =:FFLAGS = -I${MPI_HOME}/include -O3 -cpp -r8 -tp=cascadelake -target=gpu -mp=gpu -Minfo=mp -gpu=cuda12.4,cc70 -cuda:g" ${MAKEFILE}
   elif [[ "${BUILD}" == "debug" ]]; then
-    sed -i "s:FFLAGS =:FFLAGS = -I${MPI_HOME}/include -g -O0 -cpp -r8 -tp=cascadelake -target=gpu -mp=gpu -Minfo=mp -gpu=cuda12.4,cc70 -cuda -C -Mnobounds -ffpe-trap=invalid,zero,overflow,underflow,inexact -Ktrap=divz,denorm,inexact,inv,ovf,unf:g" ${MAKEFILE}
+    sed -i "s:FFLAGS =:FFLAGS = -I${MPI_HOME}/include -g -O0 -cpp -r8 -DCUDA_DEBUG -tp=cascadelake -target=gpu -mp=gpu -Minfo=mp -gpu=cuda12.4,cc70 -cuda -C -Mnobounds -ffpe-trap=invalid,zero,overflow,underflow,inexact -Ktrap=divz,denorm,inexact,inv,ovf,unf:g" ${MAKEFILE}
   fi
   sed -i "s:LIBS =:LIBS = -L${MPI_HOME}/lib -lmpi_mpifh:g" ${MAKEFILE}
 }
