cea0b85a9332647b3ba15b301dfd1bcbffd8cb38
user name <mrb@cirrus-login3.ib0.icexa.epcc.ed.ac.uk>
2025-04-21 17:26:38 +0100

    Merged the npts and ncoeffs plots.

--- tests/WMAM/scripts/plotting/plot_misc.py
+++ tests/WMAM/scripts/plotting/plot_misc.py
@@ -15,40 +15,52 @@ nx = [int(1.0/rd)*180-1 for rd in resdeg]
 
 ndatpts = np.multiply(nx, ny)
 nsampts = np.array([(shd+1)*(2*shd+1) for shd in shdeg])
-ntotpts = np.add(ndatpts, nsampts)
-lognpts = np.log10(ntotpts)
-samptfrac = 100.0*(nsampts/ntotpts)
+npts = np.add(ndatpts, nsampts)
+npts_growth = npts / npts[0]
+lognpts = np.log10(npts)
+
+samptfrac = 100.0*(nsampts/npts)
+
 ncoeffs = np.array([shd*(shd+2) for shd in shdeg])
+ncoeffs_growth = ncoeffs / ncoeffs[0]
 logncoeffs = np.log10(ncoeffs)
+
 
 shdeg_100 = [int(shd/100) if shd/100 % 1 == 0 else shd/100 for shd in shdeg]
 
 
-plt.plot(shdeg_100,logncoeffs,linestyle="-",marker="o",color="#fc8d62")
+plt.plot(shdeg_100, lognpts, linestyle='-', marker='o', color='#66c2a5', label='$N_p$')
+plt.plot(shdeg_100, logncoeffs, linestyle='-', marker='*', markersize=9, color='#fc8d62', label='$N_c$')
+
 plt.xlabel('Degree / 100')
 plt.xticks(shdeg_100, shdeg_100)
 plt.ylabel('Log10')
-plt.title(r'WMAM Coefficient Count ($N_c$)')
+plt.title(r'WMAM Point ($N_p$) and Coefficient ($N_c$) Counts')
+plt.legend(loc='upper left')
 plt.show()
-
-fig.savefig('wmam-coeff-cnt.eps', format='eps', dpi=1000)
+fig.savefig('wmam-npts-ncoeffs.eps', format='eps', dpi=1000)
 plt.show()
 plt.clf()
 fig = plt.gcf()
 
-plt.plot(shdeg_100,lognpts,linestyle="-",marker="o",color="#fc8d62")
+
+plt.plot(shdeg_100, npts_growth, linestyle='-', marker='o', color='#66c2a5', label='$N_p$ / 40400')
+plt.plot(shdeg_100, ncoeffs_growth, linestyle='-', marker='*', markersize=9, color='#fc8d62', label='$N_c$ / 145041')
+
 plt.xlabel('Degree / 100')
 plt.xticks(shdeg_100, shdeg_100)
-plt.ylabel('Log10')
-plt.title(r'WMAM Point Count ($N_p$)')
+plt.ylabel('')
+plt.title(r'WMAM Point and Coefficient Growth from Degree 200')
+plt.legend(loc='upper left')
 plt.show()
 
-fig.savefig('wmam-pt-cnt.eps', format='eps', dpi=1000)
+fig.savefig('wmam-npts-ncoeffs-growth.eps', format='eps', dpi=1000)
 plt.show()
 plt.clf()
 fig = plt.gcf()
 
-plt.plot(shdeg_100,samptfrac,linestyle="-",marker="o",color="#fc8d62")
+
+plt.plot(shdeg_100,samptfrac,linestyle='-',marker='o',color='#fc8d62')
 plt.xlabel('Degree / 100 ')
 plt.xticks(shdeg_100, shdeg_100)
 plt.ylabel('Percentage')
@@ -63,13 +75,13 @@ fig = plt.gcf()
 memreq1 = [39.123, 39.524, 42.956, 55.407, 252.025]
 memreq2 = [0.248, 0.56, 3.197, 12.858, 25.297]
 
-plt.plot(shdeg_100,memreq1,linestyle="-",marker="o",label="WMAM v1.2",color="#fc8d62")
-plt.plot(shdeg_100,memreq2,linestyle="-",marker="P",label="WMAM v3.7",color="#8da0cb")
+plt.plot(shdeg_100,memreq1,linestyle='-',marker='o',label='WMAM v1.2',color='#fc8d62')
+plt.plot(shdeg_100,memreq2,linestyle='-',marker='P',label='WMAM v3.7',color='#8da0cb')
 plt.xlabel('Degree')
 plt.xticks(shdeg_100, shdeg_100)
 plt.ylabel('Percentage')
 plt.title('WMAM Memory Requirement as percentage of 256 GB')
-plt.legend(loc="upper left")
+plt.legend(loc='upper left')
 plt.show()
 
 fig.savefig('wmam-mem-req.eps', format='eps', dpi=1000)
